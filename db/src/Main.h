2e94fb260c6fc703b422e89768f7dfeb1257e1aa
Krikal Csaladi <root@metin2.code>
2026-05-04 15:52:57 +0200

    Fix .gitignore

--- db/src/Main.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INC_MAIN_H__
-#define __INC_MAIN_H__
-
-int	Start();
-void End();
-const char * GetTablePostfix();
-const char * GetPlayerDBName();
-
-#endif
--- game/src/check_server.cpp
+++
@@ -1,4 +0,0 @@
-#include "check_server.h"
-
-std::vector<std::string> CheckServer::keys_;
-bool CheckServer::fail_ = true;
